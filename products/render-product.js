add6f259e2d41960b8c3de0c9cf81c56e7cd2f03
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-12-25 11:41:19 -0800

    render all notebooks to page

--- products/render-product.js
+++ products/render-product.js
@@ -6,26 +6,29 @@ export const renderProduct = (notebook) => {
 
   const p = document.createElement('p');
   p.textContent = notebook.name;
-  li.append(p)
 
   const img = document.createElement('img');
   img.src = `../assets/notebook-assets/${notebook.image}`;
   img.alt = notebook.description;
-  li.append(img)
 
   const descriptionPTag = document.createElement('p');
   descriptionPTag.textContent = notebook.description;
-  li.append(descriptionPTag);
 
   const pricePTag = document.createElement('p');
   pricePTag.textContent = notebook.price.toFixed(2);
   pricePTag.classList.add('price');
-  li.append(pricePTag);
 
   const button = document.createElement('button');
   button.value = notebook.id;
   button.textContent = 'Add to Cart'
-  li.append(button);
+
+  li.append(
+    p,
+    img,
+    descriptionPTag,
+    pricePTag,
+    button
+  );
 
   return li;
 }
